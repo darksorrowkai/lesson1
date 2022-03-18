8bf808c2dacd628c47dc2b787bddcf19e392a185
Max <mr_ka1@vk.com>
2022-03-18 18:23:53 +0300

    DZ

--- js/script.js
+++ js/script.js
@@ -2,5 +2,25 @@
 //console.log(answer);
 
 const numberOfFilms = [];
-numberOfFilms[0] = prompt('Сколько фильмов вы уже посмотрели?');
-document.write(numberOfFilms);
+numberOfFilms[0] = prompt('Сколько фильмов вы уже посмотрели?', '');
+//document.write(numberOfFilms);
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
+ 
