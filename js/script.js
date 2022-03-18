a7afd38f294e4402ca819a66d0e056117aad2d33
Max <mr_ka1@vk.com>
2022-03-18 14:27:11 +0300

    firsrt commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+//const answer = prompt("Вам есть 18?", "18");
+//console.log(answer);
+
+const answer = [];
+
+answer[0] = prompt('Как ваше имя?');
+answer[1] = prompt('Как ваше Фамилия?');
+answer[2] = prompt('Сколько вам лет?');
+
+document.write(answer);
