d5e292a368ece39fa54090579c6da2677da06f8a
Max <mr_ka1@vk.com>
2022-03-18 17:33:20 +0300

    new work

--- js/script.js
+++ js/script.js
@@ -1,10 +1,6 @@
 //const answer = prompt("Вам есть 18?", "18");
 //console.log(answer);
 
-const answer = [];
-
-answer[0] = prompt('Как ваше имя?');
-answer[1] = prompt('Как ваше Фамилия?');
-answer[2] = prompt('Сколько вам лет?');
-
-document.write(answer);
+const numberOfFilms = [];
+numberOfFilms[0] = prompt('Сколько фильмов вы уже посмотрели?');
+document.write(numberOfFilms);
